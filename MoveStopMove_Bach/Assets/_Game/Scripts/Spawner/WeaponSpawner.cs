60e3574b2aa5c1e3b6eb1bc6b24f4575c29c97e4
GitPab <bakamiri2016@gmail.com>
2024-08-05 04:45:15 +0700

    update

--- MoveStopMove_Bach/Assets/_Game/Scripts/Spawner/WeaponSpawner.cs
+++ MoveStopMove_Bach/Assets/_Game/Scripts/Spawner/WeaponSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
+
 
 public class WeaponSpawner : Spawner
 {
